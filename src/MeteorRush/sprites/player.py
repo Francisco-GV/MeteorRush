19f51af7758c50ae77c3fba1a9cae43052f8d452
Francisco Gasca <francisco_gasca@hotmail.com>
2025-10-22 16:26:36 -0600

    refactor(player): Encapsula lógica de jugador en clase dedicada

---
+++ src/MeteorRush/sprites/player.py
@@ -0,0 +1,59 @@
+import math
+
+import arcade
+
+from MeteorRush import constants
+
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__(
+            "assets/images/player/player_ship.png",
+            scale=constants.PLAYER_SCALING,
+        )
+
+        self.center_x = constants.SCREEN_WIDTH / 2
+        self.center_y = 50
+
+        self.up_pressed = False
+        self.down_pressed = False
+        self.left_pressed = False
+        self.right_pressed = False
+
+    def update(self, _delta_time):
+        self.change_x = 0
+        self.change_y = 0
+        self.change_angle = 0
+
+        # Lógica de rotación
+        if self.left_pressed and not self.right_pressed:
+            self.change_angle = -constants.PLAYER_ROTATION_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.change_angle = constants.PLAYER_ROTATION_SPEED
+
+        # Lógica de movimiento
+        if self.up_pressed:
+            angle_rad = math.radians(self.angle + 90)
+            self.change_x = -math.cos(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
+            self.change_y = math.sin(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
+        elif self.down_pressed:
+            angle_rad = math.radians(self.angle + 90)
+            self.change_x = math.cos(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
+            self.change_y = -math.sin(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        self.angle += self.change_angle
+
+        self._keep_within_bounds()
+
+    def _keep_within_bounds(self):
+        if self.center_x < 0:
+            self.center_x = 0
+        elif self.center_x > constants.SCREEN_WIDTH:
+            self.center_x = constants.SCREEN_WIDTH
+
+        if self.center_y < 0:
+            self.center_y = 0
+        elif self.center_y > constants.SCREEN_HEIGHT:
+            self.center_y = constants.SCREEN_HEIGHT
--- src/MeteorRush/view/game_view.py
+++ src/MeteorRush/view/game_view.py
@@ -3,13 +3,14 @@ import math
 import arcade
 
 from MeteorRush import constants
+from MeteorRush.sprites.player import Player
 
 
 class GameView(arcade.View):
 
     def __init__(self):
         super().__init__()
-        self.player_sprite = None
+        self.player = None
         self.player_list = None
 
         self.up_pressed = False
@@ -19,12 +20,9 @@ class GameView(arcade.View):
         self.right_pressed = False
 
     def setup(self):
+        self.player = Player()
         self.player_list = arcade.SpriteList()
-        image_source = "assets/images/player/player_ship.png"
-        self.player_sprite = arcade.Sprite(image_source, constants.PLAYER_SCALING)
-        self.player_sprite.center_x = self.window.width / 2
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
+        self.player_list.append(self.player)
 
     def on_show_view(self):
         arcade.set_background_color(arcade.color.AMAZON)
@@ -36,65 +34,29 @@ class GameView(arcade.View):
             self.player_list.draw()
 
     def on_update(self, delta_time):
-        if self.player_sprite and self.player_list:
-            self.player_sprite.change_x = 0
-            self.player_sprite.change_y = 0
-            self.player_sprite.change_angle = 0
-
-            # Lógica de rotación
-            if self.left_pressed and not self.right_pressed:
-                self.player_sprite.change_angle = -constants.PLAYER_ROTATION_SPEED
-            elif self.right_pressed and not self.left_pressed:
-                self.player_sprite.change_angle = constants.PLAYER_ROTATION_SPEED
-
-            # Lógica de movimiento
-            if self.up_pressed:
-                angle_rad = math.radians(self.player_sprite.angle + 90)
-                self.player_sprite.change_x = (
-                    -math.cos(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
-                )
-                self.player_sprite.change_y = (
-                    math.sin(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
-                )
-            elif self.down_pressed:
-                angle_rad = math.radians(self.player_sprite.angle + 90)
-                self.player_sprite.change_x = (
-                    math.cos(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
-                )
-                self.player_sprite.change_y = (
-                    -math.sin(angle_rad) * constants.PLAYER_MOVEMENT_SPEED
-                )
-
+        if self.player_list:
             self.player_list.update()
 
-            if self.player_sprite.left < 0:
-                self.player_sprite.left = 0
-            elif self.player_sprite.right > self.window.width:
-                self.player_sprite.right = self.window.width
-
-            if self.player_sprite.bottom < 0:
-                self.player_sprite.bottom = 0
-            elif self.player_sprite.top > self.window.height:
-                self.player_sprite.top = self.window.height
-
     def on_key_press(self, symbol, modifiers):
-        if symbol == arcade.key.UP or symbol == arcade.key.W:
-            self.up_pressed = True
-        elif symbol == arcade.key.DOWN or symbol == arcade.key.S:
-            self.down_pressed = True
+        if self.player:
+            if symbol == arcade.key.UP or symbol == arcade.key.W:
+                self.player.up_pressed = True
+            elif symbol == arcade.key.DOWN or symbol == arcade.key.S:
+                self.player.down_pressed = True
 
-        if symbol == arcade.key.LEFT or symbol == arcade.key.A:
-            self.left_pressed = True
-        elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:
-            self.right_pressed = True
+            if symbol == arcade.key.LEFT or symbol == arcade.key.A:
+                self.player.left_pressed = True
+            elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:
+                self.player.right_pressed = True
 
     def on_key_release(self, symbol, modifiers):
-        if symbol == arcade.key.UP or symbol == arcade.key.W:
-            self.up_pressed = False
-        elif symbol == arcade.key.DOWN or symbol == arcade.key.S:
-            self.down_pressed = False
+        if self.player:
+            if symbol == arcade.key.UP or symbol == arcade.key.W:
+                self.player.up_pressed = False
+            elif symbol == arcade.key.DOWN or symbol == arcade.key.S:
+                self.player.down_pressed = False
 
-        if symbol == arcade.key.LEFT or symbol == arcade.key.A:
-            self.left_pressed = False
-        elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:
-            self.right_pressed = False
+            if symbol == arcade.key.LEFT or symbol == arcade.key.A:
+                self.player.left_pressed = False
+            elif symbol == arcade.key.RIGHT or symbol == arcade.key.D:
+                self.player.right_pressed = False
