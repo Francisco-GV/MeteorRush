a65126c642f3e8d42534ff64a710d447e32779c3
Francisco Gasca <francisco_gasca@hotmail.com>
2025-10-24 00:15:13 -0600

    chore(utils): Agrega decorador para medir tiempo de ejecución

--- src/MeteorRush/utils.py
+++ src/MeteorRush/utils.py
@@ -3,6 +3,22 @@ import arcade
 from MeteorRush import constants
 
 
+def measure_time(func):
+    import time
+    def wrapper(*args, **kwargs):
+        t_start = time.perf_counter()
+
+        result = func(*args, **kwargs)
+
+        t_end = time.perf_counter()
+
+        duration = t_end - t_start
+        print(f"Función '{func.__name__}' tardó: {duration:.6f} segundos")
+
+        return result
+    return wrapper
+
+
 def scale_and_center_background(image_path: str) -> arcade.Sprite:
     texture = arcade.load_texture(image_path)
 
