2e53b1af21a667e6834fe7c96d0bbda6e4bed41f
Francisco Gasca <francisco_gasca@hotmail.com>
2025-10-30 20:42:21 -0600

    feat(juego): Agrega módulo para conectarse a base de datos

---
+++ src/MeteorRush/database.py
@@ -0,0 +1,52 @@
+import pyodbc
+
+
+class Database:
+    def __init__(self, server, database, user, password):
+        self.server = server
+        self.database = database
+        self.user = user
+        self.password = password
+        self.cnxn = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.cnxn = pyodbc.connect(
+            f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+            f"SERVER={self.server};"
+            f"UID={self.user};"
+            f"PWD={self.password};"
+            f"DATABASE={self.database};"
+            f"TrustServerCertificate=yes;"
+        )
+        self.cursor = self.cnxn.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.cnxn:
+            self.cnxn.close()
+
+    def upload_player_data(
+        self, player_name, score, duration, destroyed_asteroid_counts
+    ):
+        self.cursor.execute(
+            (
+                "INSERT INTO player ("
+                "name,"
+                "score,"
+                "duration,"
+                "tiny_asteroids_destroyed,"
+                "small_asteroids_destroyed,"
+                "medium_asteroids_destroyed,"
+                "large_asteroids_destroyed"
+                ") VALUES (?, ?, ?, ?, ?, ?, ?)"
+            ),
+            player_name,
+            score,
+            duration,
+            destroyed_asteroid_counts["tiny"],
+            destroyed_asteroid_counts["small"],
+            destroyed_asteroid_counts["medium"],
+            destroyed_asteroid_counts["large"],
+        )
+        self.cnxn.commit()
